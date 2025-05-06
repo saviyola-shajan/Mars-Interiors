d621a09dc75613514272a28fab17f9d73109da39
saviyola-shajan <saviyolashajan69@gmail.com>
2025-05-06 20:45:50 +0530

    corrcet errors in footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,21 +3,23 @@ import { BiPhoneCall } from "react-icons/bi";
 import { FaLocationDot } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
 import { FaWhatsapp } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
     <footer className="py-12 px-8 sm:px-10 md:px-16 lg:px-24">
       {/* Footer Grid */}
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 md:gap-16 text-left">
-        
         {/* Location Section */}
         <div className="space-y-4">
           <h3 className="text-[#000000] text-2xl font-medium">Location</h3>
           <p className="flex items-start text-lg text-[#0B0B0B] space-x-3">
             <FaLocationDot className="mt-1 text-[#17A396]" />
             <span>
-              Mars Interiors, 24/7 Global Spaces, Peters Nine,<br />
-              Near Josco Jewellers, Thodupuzha, Kerala<br />
+              Mars Interiors, 24/7 Global Spaces, Peters Nine,
+              <br />
+              Near Josco Jewellers, Thodupuzha, Kerala
+              <br />
               PinCode: 685584
             </span>
           </p>
@@ -26,11 +28,37 @@ const Footer = () => {
         {/* Quick Links Section */}
         <div className="space-y-4">
           <h3 className="text-[#000000] text-2xl font-medium">Quick Links</h3>
+
           <ul className="space-y-2 text-lg text-[#0B0B0B]">
-            <li><a href="/" className="hover:text-[#17A396] transition-colors">Home</a></li>
-            <li><a href="/aboutus" className="hover:text-[#17A396] transition-colors">About Us</a></li>
-            <li><a href="/services" className="hover:text-[#17A396] transition-colors">Services</a></li>
-            <li><a href="/contact" className="hover:text-[#17A396] transition-colors">Contact</a></li>
+            <li>
+              <Link to="/" className="hover:text-[#17A396] transition-colors">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/aboutus"
+                className="hover:text-[#17A396] transition-colors"
+              >
+                About Us
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/services"
+                className="hover:text-[#17A396] transition-colors"
+              >
+                Services
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/contact"
+                className="hover:text-[#17A396] transition-colors"
+              >
+                Contact
+              </Link>
+            </li>
           </ul>
         </div>
 
@@ -40,24 +68,41 @@ const Footer = () => {
           <ul className="space-y-3 text-lg text-[#0B0B0B]">
             <li className="flex items-center space-x-3">
               <IoIosMail className="w-6 h-6 text-[#17A396]" />
-              <a href="mailto:info@marsinterios.com" className="hover:underline">info@marsinterios.com</a>
+              <a
+                href="mailto:info@marsinterios.com"
+                className="hover:underline"
+              >
+                info@marsinterios.com
+              </a>
             </li>
             <li className="flex items-center space-x-3">
               <FaWhatsapp className="w-6 h-6 text-[#17A396]" />
-              <a href="https://wa.me/9567876460" className="hover:underline">+91 9567876460</a>
+              <a href="https://wa.me/9567876460" className="hover:underline">
+                +91 9567876460
+              </a>
             </li>
             <li className="flex items-center space-x-3">
               <BiPhoneCall className="w-6 h-6 text-[#17A396]" />
-              <a href="tel:+919562978418" className="hover:underline">+91 9562978418</a>
+              <a href="tel:+919562978418" className="hover:underline">
+                +91 9562978418
+              </a>
             </li>
           </ul>
 
           {/* Social Icons */}
           <div className="flex space-x-5 mt-4">
-            <a href="https://www.instagram.com/mars_interiors_.co.in?igsh=MzRycTVjbndlNm90" target="_blank" className="text-2xl text-[#17A396] transition-colors">
+            <a
+              href="https://www.instagram.com/mars_interiors_.co.in?igsh=MzRycTVjbndlNm90"
+              target="_blank"
+              className="text-2xl text-[#17A396] transition-colors"
+            >
               <FaInstagram className="w-8 h-8" />
             </a>
-            <a href="https://wa.me/9567876460" target="_blank" className="text-2xl text-[#17A396]  transition-colors">
+            <a
+              href="https://wa.me/9567876460"
+              target="_blank"
+              className="text-2xl text-[#17A396]  transition-colors"
+            >
               <FaWhatsapp className="w-8 h-8" />
             </a>
           </div>
